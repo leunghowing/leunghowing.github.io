904be00b9323b623f16fb347aa149e5c114653a9
Owen HW Leung <owen.hw.leung@ibm.com>
2022-11-18 17:03:02 +0800

    d

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var datetimenow = "";
 
-function addupRoutes(resp, data, number){
+function addRoute(resp, data, number){
     resp = resp + "<tr><td>" + data['data'][number]['route'] + "</td><td>"+ data['data'][number]['eta'].substring(11,16) + "</td><td><span class='ziel'>" +  data['data'][number]['dest_tc'] + "</span></td>";
     var diff =(new Date(Date.parse(data['data'][number]['eta'].substring(0,19))).getTime() - new Date(Date.parse(datetimenow)).getTime()) / 1000;
     diff /= 60;
@@ -18,6 +18,27 @@ function addupRoutes(resp, data, number){
     return resp;
 }
 
+function addUpRoutes(data){
+    var resp = "";
+    for(let i=0; i < data.length; i++){
+        resp = resp + "<tr><td>" + data[i]['route'] + "</td><td>"+ data[i]['eta'].substring(11,16) + "</td><td><span class='ziel'>" +  data[i]['dest_tc'] + "</span></td>";
+        var diff =(new Date(Date.parse(data[i]['eta'].substring(0,19))).getTime() - new Date(Date.parse(datetimenow)).getTime()) / 1000;
+        diff /= 60;
+        if( diff < 1 ){
+            resp = resp + "<td></td>";
+        }
+        else if(Math.round(diff) < 60){
+            resp = resp + "<td>" + Math.round(diff) + "<span style='font-size: 10px;'> Min.</span></td>";
+        }
+        else{
+            resp = resp + "<td>" + Math.round(diff/60) + "<span style='font-size: 10px;'>  Std.</span></td>";
+        }
+        //console.log(diff);
+        resp = resp + "</tr>"; 
+    }
+    return resp;
+}
+
 function getStop(stopids,routes){
     var response = "";
     for(var k=0; k < stopids.length; k++){
@@ -32,12 +53,12 @@ function getStop(stopids,routes){
                     if(data['data'][i]['eta']!=null){
                         if(routes){
                                 if(routes.includes(data['data'][i]['route'])){
-                                    response = addupRoutes(response, data, i);
+                                    response = addRoute(response, data, i);
                                     j++;
                                 }
                         }
                         else{
-                            response = addupRoutes(response, data, i);
+                            response = addRoute(response, data, i);
                             j++;
                         }
                     }
@@ -65,12 +86,12 @@ function getStopAsync(element, stopids, routes){
                     if(data['data'][i]['eta']!=null){
                         if(routes){
                                 if(routes.includes(data['data'][i]['route'])){
-                                    response = addupRoutes(response, data, i);
+                                    response = addRoute(response, data, i);
                                     j++;
                                 }
                         }
                         else{
-                            response = addupRoutes(response, data, i);
+                            response = addRoute(response, data, i);
                             j++;
                         }
                     }
@@ -88,80 +109,65 @@ function getStopAsync(element, stopids, routes){
 
 }
 
-function getSingleRouteStop(stopid,route){
+function getStopAsyncSorted(element, stopids, routes){
+    var unsorted = [];
     var response = "";
-    $.ajax({
-        type: 'GET',
-        url:'https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/'+ stopid,
-        dataType: 'json',
-        async : false,
-        success: function(data){
-            let j = 1;
-            for(var i = 0; i < data['data'].length; i++ ){ 
-                if(data['data'][i]['route'] == route){
-                    if(response!=""){
-                        response += "<br>";
-                    }
-                    response = response + data['data'][i]['eta'].substring(11,16);
-                    //console.log(response);
-                    var diff =(new Date(Date.parse(data['data'][i]['eta'].substring(0,19))).getTime() - new Date(Date.parse(datetimenow)).getTime()) / 1000;
-                    diff /= 60;
-                    if( diff < 1 ){
-                        response = response + " (0 Min)";
-                    }
-                    else{
-                        response = response +" (" + Math.round(diff) + " Min)";
+    for(var k=0; k < stopids.length; k++){
+        $.ajax({
+            type: 'GET',
+            url:'https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/'+ stopids[k],
+            dataType: 'json',
+            success: function(data){
+                let j = 1;
+                for(var i = 0; i < data['data'].length; i++ ){ 
+                    if(data['data'][i]['eta']!=null){
+                        if(routes){
+                                if(routes.includes(data['data'][i]['route'])){
+                                    let repeated = 0;
+                                    for(let l=0; l < unsorted.length; l ++){
+                                        if(unsorted[l]['route'] == data['data'][i]['route'] && unsorted[l]['eta'] == data['data'][i]['eta'] && unsorted[l]['eta_seq'] == data['data'][i]['eta_seq']){
+                                            repeated = 1;
+                                        }
+                                    }
+                                    if(!repeated){
+                                        unsorted[j-1] = data['data'][i]; 
+                                        j++;
+                                    }
+                                    
+                                }
+                        }
+                        else{
+                            let repeated = 0;
+                            for(let l=0; l < unsorted.length; l ++){
+                                if(unsorted[l]['route'] == data['data'][i]['route'] && unsorted[l]['eta'] == data['data'][i]['eta'] && unsorted[l]['eta_seq'] == data['data'][i]['eta_seq']){
+                                    repeated = 1;
+                                }
+                            }
+                            if(!repeated){
+                                unsorted[j-1] = data['data'][i]; 
+                                j++;
+                            }
+                        }
                     }
                 }
+                unsorted.sort(function (a, b) {
+                    return a.eta.localeCompare(b.eta);
+                });
+                response = addUpRoutes(unsorted);
+                if(response == ""){
+                    document.getElementById(element).innerHTML = "Keine Daten";
+                }
+                else{
+                    document.getElementById(element).innerHTML = tablehead + response + "</table>";
+                }
+                
             }
-        }
-    });
-    //console.log("hhfuhfuhfuhfueh");
-
-    if(response == ""){
-        response = "-";
+        });
     }
-    return response;
+
 }
 
-function getSingleRouteStops(route, bound, seq){
-    var response = [];
-    $.ajax({
-        type: 'GET',
-        url:'https://data.etabus.gov.hk/v1/transport/kmb/route-eta/'+ route + "/1",
-        dataType: 'json',
-        async : false,
-        success: function(data){
-            let j = 0;
-            for(var i = 0; i < data['data'].length; i++ ){ 
-                if(data['data'][i]['dir'] == bound && data['data'][i]['seq']==seq[j]){
-                    if(response[j]==null){
-                        response[j] = ""; 
-                    }
-                    if(response[j]!=""){
-                        response[j] += "<br>";
-                    }
-                    var diff =(new Date(Date.parse(data['data'][i]['eta'].substring(0,19))).getTime() - new Date(Date.parse(datetimenow)).getTime()) / 1000;
-                    diff /= 60;
-                    if( diff < 0){
-                    }
-                    else if( diff < 1 ){
-                        response[j] = response[j] + data['data'][i]['eta'].substring(11,16) + " (0 Min)";
-                    }
-                    else{
-                        response[j] = response[j] +  data['data'][i]['eta'].substring(11,16) + " (" + Math.round(diff) + " Min)";
-                    }
-                    //console.log(response[j]);
-                    if(data['data'][i+1]['seq']!=null && data['data'][i+1]['seq']!=data['data'][i]['seq']){
-                        j++;
-                    }
-                }
-            }
-            //console.log(response);
-        }
-    });
-    return response;
-}
+
 function getSingleRouteStopsAsync(route, bound, seq, stopnames){
     var response = [];
     $.ajax({
